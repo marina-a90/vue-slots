58efe674130fcbc2a4d9445a47d24da46b422a13
Marina <marinaandjelkovic456@gmail.com>
2019-03-21 20:01:02 +0100

    slotovi

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Slots</h1>
+    <car brand="mercedes" model="limuzina"/>
+
+    <hr>
+
+    <car brand="opel" model="kadet">
+      <!-- <car brand="opel"> -->
+      <p style="color: green">Kadet</p>
+    </car>
+
+
+
+    <base-layout>
+
+      <template slot="header">
+        <h1>Header</h1>
+        <p>bla</p>
+      </template>
+
+      <template>
+        <h3>default</h3>
+      </template>
+
+      <template slot="footer">
+        <h6>footer</h6>
+      </template>
+
+    </base-layout>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Car from "./components/Car";
+import BaseLayout from "./components/BaseLayout";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Car,
+    BaseLayout
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Car.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        
+        <p style="color: red">{{ brand }}</p>
+
+        <slot>
+            <p style="color: blue">{{ model }}</p>
+        </slot>
+
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    model: {
+      type: String
+    },
+    brand: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style>
+</style>
